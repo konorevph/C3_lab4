4e91b27d23a467fffadc56083c2abcc9c9371b4e
konorevph <regouserk@mail.ru>
2024-01-09 00:00:53 +0300

    Наброски варианта на курсорах

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import ADTTree.Cursors.Tree;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree tree = new Tree();
+        Tree tree2 = new Tree();
+        tree = tree.CREATE("label");
+        tree2 = tree2.CREATE("label2");
+//        Tree.printArray();
+        tree = tree.CREATE("label3", tree2);
+        System.out.println(tree);
+        Tree.printArray();
     }
 }
